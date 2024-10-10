9cc04c7c5e9ce07ab11028f5dba2a7e38b8f07a2
profrafamatos <rcmm@cesar.school>
2024-10-10 09:26:20 -0300

    Arquivos necessários para resolver o exercício

---
+++ index.js
@@ -0,0 +1,11 @@
+let primeiraParte = "Aprender "
+let segundaParte = "Javascript "
+let terceiraParte = "é divertido!"
+
+//Instrução: concatene as variáveis usando o método concat()
+/**** Escreva o código abaixo 👇******/
+
+
+/**** Escreva o código acima ☝️******/
+
+console.log(fraseCompleta)
